ffdfaed68c2fa656415593ad3c782154ce551847
Moy Wu <immoywu@gmail.com>
2020-09-19 23:14:43 +0800

    no message

--- v2/scripts/default/cm2-map.js
+++ v2/scripts/default/cm2-map.js
@@ -5,7 +5,16 @@ var mapstyle = [
         "elementType": "labels.text.fill",
         "stylers": [
             {
-                "color": "#6195a0"
+                "color": "#444444"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative.locality",
+        "elementType": "all",
+        "stylers": [
+            {
+                "gamma": "1.00"
             }
         ]
     },
@@ -23,7 +32,79 @@ var mapstyle = [
         "elementType": "geometry.fill",
         "stylers": [
             {
-                "color": "#ffffff"
+                "saturation": "1"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape.man_made",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "lightness": "23"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape.natural",
+        "elementType": "all",
+        "stylers": [
+            {
+                "color": "#c7dba8"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape.natural",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "saturation": "-6"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape.natural",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "lightness": "30"
+            },
+            {
+                "saturation": "13"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape.natural.landcover",
+        "elementType": "all",
+        "stylers": [
+            {
+                "saturation": "1"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape.natural.landcover",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "saturation": "7"
+            },
+            {
+                "lightness": "11"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape.natural.terrain",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "saturation": "6"
+            },
+            {
+                "lightness": "6"
             }
         ]
     },
@@ -33,18 +114,6 @@ var mapstyle = [
         "stylers": [
             {
                 "visibility": "off"
-            }
-        ]
-    },
-    {
-        "featureType": "poi.park",
-        "elementType": "geometry.fill",
-        "stylers": [
-            {
-                "color": "#e6f3d6"
-            },
-            {
-                "visibility": "on"
             }
         ]
     },
@@ -77,37 +146,22 @@ var mapstyle = [
         "elementType": "geometry.fill",
         "stylers": [
             {
-                "color": "#f4d2c5"
+                "color": "#f19830"
             },
             {
                 "visibility": "simplified"
+            },
+            {
+                "weight": "3"
             }
         ]
     },
     {
         "featureType": "road.highway",
-        "elementType": "labels.text",
+        "elementType": "geometry.stroke",
         "stylers": [
             {
-                "color": "#4e4e4e"
-            }
-        ]
-    },
-    {
-        "featureType": "road.arterial",
-        "elementType": "geometry.fill",
-        "stylers": [
-            {
-                "color": "#f4f4f4"
-            }
-        ]
-    },
-    {
-        "featureType": "road.arterial",
-        "elementType": "labels.text.fill",
-        "stylers": [
-            {
-                "color": "#787878"
+                "color": "#f19830"
             }
         ]
     },
@@ -134,19 +188,10 @@ var mapstyle = [
         "elementType": "all",
         "stylers": [
             {
-                "color": "#eaf6f8"
+                "color": "#a1cbfd"
             },
             {
                 "visibility": "on"
-            }
-        ]
-    },
-    {
-        "featureType": "water",
-        "elementType": "geometry.fill",
-        "stylers": [
-            {
-                "color": "#eaf6f8"
             }
         ]
     }
--- v2/scripts/default/ui.js
+++ v2/scripts/default/ui.js
@@ -1,79 +1,77 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
 	$('.jqimgFill').imgLiquid();
 
 	$('a[data-rel^=lightcase]').lightcase();
-	
-		var wow = new WOW(
-			{
-				boxClass:     'wow',  
-				offset:       10   
-			}
-		);
-		wow.init();
+
+	var wow = new WOW({
+		boxClass: 'wow',
+		offset: 10
+	});
+	wow.init();
 
 
 	/* ==========================================================================
     * 變數定義
   ==========================================================================*/
-  resizeCss();
+	resizeCss();
 
 
 	/* ==========================================================================
 		[header]
  	==========================================================================*/
-	$('#header').each(function() {
-		$(this).css("visibility","visible");
-		$(".menu-toggle").click(function() {
+	$('#header').each(function () {
+		$(this).css("visibility", "visible");
+		$(".menu-toggle").click(function () {
 			$(this).toggleClass('active');
 			$(this).next(".menu").toggleClass('active');
 		});
 
-		if (jQuery(window).width() < 991) { 
-			$('.sub-toggle').click(function(){
-                $(this).find('.sub-nav').slideToggle();
-            });
+		if (jQuery(window).width() < 991) {
+			$('.sub-toggle').click(function () {
+				$(this).find('.sub-nav').slideToggle();
+			});
 		}
 	});
 
 	$(".goTop").click(function () {
-			$("html,body").animate({
-					scrollTop: 0
-			}, 1300, 'easeInOutCubic');
-			return false;
+		$("html,body").animate({
+			scrollTop: 0
+		}, 1300, 'easeInOutCubic');
+		return false;
 	});
 
-	$('.lang-btn').click(function(){
+	$('.lang-btn').click(function () {
 		$('.lang-nav').slideToggle();
 		$('.member-nav').slideUp();
 	});
 
-	$('.member-btn').click(function(){
+	$('.member-btn').click(function () {
 		$('.member-nav').slideToggle();
 		$('.lang-nav').slideUp();
 	});
 
-  /* ==========================================================================
+	/* ==========================================================================
     * 共用
 	==========================================================================*/
 
-	$("a.anchor").on('click', function(event) {
+	$("a.anchor").on('click', function (event) {
 
 		if (this.hash !== "") {
-		  event.preventDefault();
-	
-		  // Store hash
-		  var hash = this.hash;
+			event.preventDefault();
 
-		  $('html, body').animate({
-			scrollTop: $(hash).offset().top - 90
-		  }, 800, function(){
-	   
-			window.location.hash = hash;
-		  });
+			// Store hash
+			var hash = this.hash;
+
+			$('html, body').animate({
+				scrollTop: $(hash).offset().top - 90
+			}, 800, function () {
+
+				window.location.hash = hash;
+			});
 		} // End if
 	});
-	
+
 	$('.l-chartNews .slider').slick({
 		fade: true,
 		dots: true,
@@ -83,35 +81,34 @@ $(document).ready(function() {
 	});
 
 	$('.slider_qty3 .slider').slick({
-			dots: false,
-			infinite: false,
-			slidesToShow: 3,
-			slidesToScroll: 3,
-			responsive: [
-				{
-					breakpoint: 1100,
-					settings: {
-						slidesToShow: 2,
-						slidesToScroll: 2
-					}
-				},
-				{
-					breakpoint: 700,
-					settings: {
-						slidesToShow: 1,
-						slidesToScroll: 1
-					}
-				},
-				{
-					breakpoint: 480,
-					settings: {
-						arrows: false,
-						dots: true,
-						slidesToShow: 1,
-						slidesToScroll: 1
-					}
+		dots: false,
+		infinite: false,
+		slidesToShow: 3,
+		slidesToScroll: 3,
+		responsive: [{
+				breakpoint: 1100,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2
 				}
-			]
+			},
+			{
+				breakpoint: 700,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					arrows: false,
+					dots: true,
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+			}
+		]
 	});
 
 	$('#app .new-arrival .slider').slick({
@@ -128,18 +125,16 @@ $(document).ready(function() {
 		arrows: true,
 		autoplay: true,
 		autoplaySpeed: 5000,
-		responsive: [
-			{
-				breakpoint: 767,
-				settings: {
-					dots: true,
-					arrows: false
-				}
+		responsive: [{
+			breakpoint: 767,
+			settings: {
+				dots: true,
+				arrows: false
 			}
-		]
+		}]
 	});
 
-	$('.select_nav').click(function(){
+	$('.select_nav').click(function () {
 		$('.select_submenu').slideToggle();
 	});
 	// $('.select_submenu li').click(function(){
@@ -150,36 +145,34 @@ $(document).ready(function() {
     * page
 	==========================================================================*/
 
-	$('#about').each(function(){
-		$('.team-item').click(function(){
+	$('#about').each(function () {
+		$('.team-item').click(function () {
 			$(this).children('.team-hover').toggleClass('active').parent().siblings().children('.team-hover').removeClass('active');
 		});
 	});
 
-	$('#seminar').each(function(){
+	$('#seminar').each(function () {
 		$('#header').addClass('nobg');
-		$(window).scroll(function (){
+		$(window).scroll(function () {
 			if ($(this).scrollTop() < 500) {
 				$('#header').addClass('nobg');
 			} else {
 				$('#header').removeClass('nobg');
 			}
 		});
-		if (jQuery(window).width() < 991) { 
+		if (jQuery(window).width() < 991) {
 			$('.seminar-advantage .slider').slick({
 				dots: false,
 				infinite: false,
 				slidesToShow: 2,
 				slidesToScroll: 2,
-				responsive: [
-					{
-						breakpoint: 767,
-						settings: {
-							slidesToShow: 1,
-							slidesToScroll: 1
-						}
+				responsive: [{
+					breakpoint: 767,
+					settings: {
+						slidesToShow: 1,
+						slidesToScroll: 1
 					}
-				]
+				}]
 			});
 		}
 
@@ -189,78 +182,74 @@ $(document).ready(function() {
 			slidesToShow: 2,
 			slidesToScroll: 2,
 			initialSlide: 1,
-			responsive: [
-				{
-					breakpoint: 991,
-					settings: {
-						slidesToShow: 1,
-						slidesToScroll: 1
-					}
+			responsive: [{
+				breakpoint: 991,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
 				}
-			]
+			}]
 		});
 	});
 
-	$('#seminar-edm').each(function(){
+	$('#seminar-edm').each(function () {
 		$('.seminar_news .slider').slick({
 			dots: false,
 			arrows: true,
 			slidesToShow: 2,
 			slidesToScroll: 2,
 			initialSlide: 1,
-			responsive: [
-				{
-					breakpoint: 991,
-					settings: {
-						slidesToShow: 1,
-						slidesToScroll: 1
-					}
+			responsive: [{
+				breakpoint: 991,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
 				}
-			]
+			}]
 		});
 
 		$('.footer-nav').hide();
 	});
 
-	$('#login').each(function(){
+	$('#login').each(function () {
 		$('#header').addClass('only_nobg');
 		$('#header').addClass('text_w');
 		$('#header .link-nav > ul').hide();
 		$('.footer-nav').hide();
 		$('footer').hide();
 	});
-	
-	$('#login.activity').each(function(){
+
+	$('#login.activity').each(function () {
 		if (jQuery(window).width() > 992) {
-			$('.login-info').mCustomScrollbar({ 
+			$('.login-info').mCustomScrollbar({
 				scrollbarPosition: 'outside',
 				theme: "minimal-dark"
 			});
-		 }
+		}
 	});
 
 
-	$('#realestate').each(function(){
-		if (jQuery(window).width() < 991) { 
-			$('.realestate_btn').click(function(){
+	$('#realestate').each(function () {
+		if (jQuery(window).width() < 991) {
+			$('.realestate_btn').click(function () {
 				$('.realestate_card').show();
 				$('.pop_bg').show();
 			});
-			$('.pop_bg').click(function(){
+			$('.pop_bg').click(function () {
 				$('.realestate_card').hide();
 				$('.pop_bg').hide();
 			});
 		}
-		$(window).scroll(function() {
+		$(window).scroll(function () {
 			if ($(this).scrollTop() > 500) {
-			  $('.realestate_card').addClass('active');
+				$('.realestate_card').addClass('active');
 			} else {
 				$('.realestate_card').removeClass('active');
 			}
 		});
 	});
 
-	$(window).scroll(function() {
+	$(window).scroll(function () {
 		if ($(this).scrollTop() > 400) {
 			$('.mydetail_card').addClass('active');
 			$('.wallet_inner').addClass('active');
@@ -270,76 +259,76 @@ $(document).ready(function() {
 		}
 	});
 
-	$('#wallet').each(function(){
+	$('#wallet').each(function () {
 		$('.footer-nav').addClass('bg-light');
-		$('.record_item').click(function(){
+		$('.record_item').click(function () {
 			$(this).find('.record_buy_detail_inner').slideToggle();
 		});
 	});
-	$('#investment').each(function(){
+	$('#investment').each(function () {
 		$('.footer-nav').addClass('bg-light');
 
-		$('.investment-detail_item').click(function(){
+		$('.investment-detail_item').click(function () {
 			$(this).find('.my_buylist').slideToggle();
 		});
 	});
 
-	$('#markplace').each(function(){
-		$('#markplace_add').click(function(){
+	$('#markplace').each(function () {
+		$('#markplace_add').click(function () {
 			$('#markplace_pop').show();
 			$('.pop_bg').show();
 		});
-		$('.pop_bg').click(function(){
+		$('.pop_bg').click(function () {
 			$('#markplace_pop').hide();
 			$('.pop_bg').hide();
 		});
-		$('#markplace_pop .close').click(function(){
+		$('#markplace_pop .close').click(function () {
 			$('#markplace_pop').hide();
 			$('.pop_bg').hide();
 		});
 	});
 
-	$('#market_list').click(function(){
+	$('#market_list').click(function () {
 		$(this).addClass('active');
 		$('#market_block').removeClass('active');
 		$('.all-estate').addClass('market_list');
 		$('.all-estate').removeClass('market_block');
 	});
-	$('#market_block').click(function(){
+	$('#market_block').click(function () {
 		$(this).addClass('active');
 		$('#market_list').removeClass('active');
 		$('.all-estate').addClass('market_block');
 		$('.all-estate').removeClass('market_list');
 	});
 
-	$('#market_Filter').each(function(){
-		$('#market_Filter').ckFilter({
-			reset: true,
-			multiple: false,
-			animation: false
-		});
-	});
-	
-	$(window).resize(function() {
+	// $('#market_Filter').each(function () {
+	// 	$('#market_Filter').ckFilter({
+	// 		reset: true,
+	// 		multiple: false,
+	// 		animation: false
+	// 	});
+	// });
+
+	$(window).resize(function () {
 		if (jQuery(window).width() < 767) {
 			$('.all-estate').removeClass('market_list');
 			$('.all-estate').addClass('market_block');
 		}
 	});
-	
-	$('.ckFilter-button').click(function(){
-		$(this).addClass('active').siblings().removeClass('active');
+
+	$('.ckFilter-button').click(function () {
+		$(this).toggleClass('active');
 	});
 
-	$('#cm2-house .close-btn').click(function(){
+	$('#cm2-house .close-btn').click(function () {
 		$('#cm2-house').removeClass('active');
 	});
 
-	$('.search-btn').click(function(){
+	$('.search-btn').click(function () {
 		$('.search-row').slideToggle();
 	});
 
-	$('.market-item').each(function(){
+	$('.market-item').each(function () {
 		var market_head_offsetTop = $('.market-head').offset();
 		var chartNews_offsetTop = $('.l-chartNews').offset();
 		$(window).scroll(function () {
@@ -351,84 +340,109 @@ $(document).ready(function() {
 		});
 	});
 
+	$('#notice').each(function () {
+		$('#read').change(function () {
+			if ($(this).is(":checked")) {
+				$('.notice-item:not(.readed)').addClass('haveRead');
+			} else {
+				$('.notice-item:not(.readed)').removeClass('haveRead');
+			}
+		});
+	});
+
+	$('#setting').each(function () {
+		$('.setting_btn').click(function () {
+			$('#setting_feeback').show();
+			$('.pop_bg').show();
+		});
+		$('.pop_bg').click(function () {
+			$('#setting_feeback').hide();
+			$('.pop_bg').hide();
+		});
+		$('#setting_feeback .close,#setting_feeback .btn').click(function () {
+			$('#setting_feeback').hide();
+			$('.pop_bg').hide();
+		});
+	});
+
 
 	/* ==========================================================================
 			IE 9 不支援 placeholder
 	==========================================================================*/
 	(function ($) {
-   $.support.placeholder = ('placeholder' in document.createElement('input'));
+		$.support.placeholder = ('placeholder' in document.createElement('input'));
 	})(jQuery);
 
 	//fix for IE7 and IE8
 	$(function () {
-    if (!$.support.placeholder) {
-	    $("[placeholder]").focus(function () {
-	      if ($(this).val() == $(this).attr("placeholder")) $(this).val("");
-	     }).blur(function () {
-	  	  if ($(this).val() == "") $(this).val($(this).attr("placeholder"));
-		}).blur();
+		if (!$.support.placeholder) {
+			$("[placeholder]").focus(function () {
+				if ($(this).val() == $(this).attr("placeholder")) $(this).val("");
+			}).blur(function () {
+				if ($(this).val() == "") $(this).val($(this).attr("placeholder"));
+			}).blur();
 
-		$("[placeholder]").parents("form").submit(function () {
-	    $(this).find('[placeholder]').each(function() {
-	      if ($(this).val() == $(this).attr("placeholder")) {
-	        $(this).val("");
-	        }
-	     });
+			$("[placeholder]").parents("form").submit(function () {
+				$(this).find('[placeholder]').each(function () {
+					if ($(this).val() == $(this).attr("placeholder")) {
+						$(this).val("");
+					}
+				});
 			});
-	  }
+		}
 	});
 
 	/* ==========================================================================
 			頁籤
 	==========================================================================*/
 
-	$(function() {
+	$(function () {
 		var _showTab = 0;
 		var $defaultLi = $('ul.cutover li').eq(_showTab).addClass('active');
 		$($defaultLi.find('a').attr('href')).siblings().removeClass('show');
 
-		$('ul.cutover li').click(function() {
+		$('ul.cutover li').click(function () {
 			var $this = $(this),
 				_clickTab = $this.find('a').attr('href');
 			$this.addClass('active').siblings('.active').removeClass('active');
 			$(_clickTab).stop(false, true).addClass('show').siblings().removeClass('show');
 
 			return false;
-		}).find('a').focus(function() {
+		}).find('a').focus(function () {
 			this.blur();
 		});
 	});
 
-  /* ==========================================================================
-    * 螢幕縮放動作
-  ==========================================================================*/
-  $(window).resize(function() {
-  	resizeCss();
-  });
+	/* ==========================================================================
+	  * 螢幕縮放動作
+	==========================================================================*/
+	$(window).resize(function () {
+		resizeCss();
+	});
 
-  function resizeCss() {
+	function resizeCss() {
 
-  }
-  /*document END*/
+	}
+	/*document END*/
 });
 
 
 var inputs = document.querySelectorAll('.file-input')
 
 for (var i = 0, len = inputs.length; i < len; i++) {
-  customInput(inputs[i])
+	customInput(inputs[i])
 }
 
-function customInput (el) {
-  const fileInput = el.querySelector('[type="file"]')
-  const label = el.querySelector('[data-js-label]')
-  
-  fileInput.onchange =
-  fileInput.onmouseout = function () {
-    if (!fileInput.value) return
-    
-    var value = fileInput.value.replace(/^.*[\\\/]/, '')
-    el.className += ' -chosen'
-    label.innerText = value
-  }
+function customInput(el) {
+	const fileInput = el.querySelector('[type="file"]')
+	const label = el.querySelector('[data-js-label]')
+
+	fileInput.onchange =
+		fileInput.onmouseout = function () {
+			if (!fileInput.value) return
+
+			var value = fileInput.value.replace(/^.*[\\\/]/, '')
+			el.className += ' -chosen'
+			label.innerText = value
+		}
 }
